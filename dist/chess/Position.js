6f300942818219695ce7a22fabeccf132bb45312
Ed Soria <edsoria76@gmail.com>
2025-07-29 09:41:57 -0600

    Remove dist folder from tracking

--- dist/chess/Position.js
+++
@@ -1,3 +0,0 @@
-export function positionsEqual(a, b) {
-    return a.x === b.x && a.y === b.y;
-}
--- dist/index.js
+++
@@ -1,6 +0,0 @@
-import { Game } from './chess/Game';
-const gameContainer = document.getElementById('game');
-if (!gameContainer)
-    throw new Error('#game not found');
-const game = new Game(gameContainer);
-game.start();
