0cfc4ff27b94f57db92e2ef15faeccef604ccc65
MYS_158 <emwyes158@gmail.com>
2025-07-30 10:44:50 -0600

    Added all sliding pieces

---
+++ src/core/pieces/Bishop.ts
@@ -0,0 +1,12 @@
+﻿import { SlidingPiece } from "./SlidingPiece";
+import { Position } from "../move";
+import { Color, Category } from "./pieceTypes";
+
+export class Bishop extends SlidingPiece {
+    static directions = [
+        { dx: 1, dy: 1 }, { dx: 1, dy: -1 }, { dx: -1, dy: 1 }, { dx: -1, dy: -1 }
+    ];
+    public category: Category = 'bishop';
+    public symbol = this.color === 'white' ? '♗' : '♝';
+    constructor(color: Color, pos: Position) { super(color, pos, Bishop.directions); }
+}
---
+++ src/core/pieces/Rook.ts
@@ -0,0 +1,12 @@
+﻿import { SlidingPiece } from "./SlidingPiece";
+import { Position } from "../move";
+import { Color, Category } from "./pieceTypes";
+
+export class Rook extends SlidingPiece {
+    static directions = [
+        { dx: 1, dy: 0 }, { dx: -1, dy: 0 }, { dx: 0, dy: 1 }, { dx: 0, dy: -1 }
+    ];
+    public category: Category = 'rook';
+    public symbol = this.color === 'white' ? '♖' : '♜';
+    constructor(color: Color, pos: Position) { super(color, pos, Rook.directions); }
+}
