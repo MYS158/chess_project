804bf3745be8c6fbd750488f7f6f42253d141250
MYS_158 <emwyes158@gmail.com>
2025-07-30 11:06:25 -0600

    Fixed knight

--- src/core/pieces/Knight.ts
+++ src/core/pieces/Knight.ts
@@ -4,17 +4,16 @@ import { isInsideBoard } from "../utils/isInsideBoard";
 import { Category } from "./pieceTypes";
 import { Board } from "../board";
 
-const knightShifts = [
-    { dx: 1, dy: 2 }, { dx: 2, dy: 1 }, { dx: 2, dy: -1 }, { dx: 1, dy: -2 },
-    { dx: -1, dy: -2 }, { dx: -2, dy: -1 }, { dx: -2, dy: 1 }, { dx: -1, dy: 2 }
-];
-
 export class Knight extends Piece {
     public category: Category = 'knight';
     public symbol = this.color === 'white' ? '♘' : '♞';
+    directions = [
+        { dx: 1, dy: 2 }, { dx: 2, dy: 1 }, { dx: 2, dy: -1 }, { dx: 1, dy: -2 },
+        { dx: -1, dy: -2 }, { dx: -2, dy: -1 }, { dx: -2, dy: 1 }, { dx: -1, dy: 2 }
+    ];
 
     public getLegalMoves(board: Board): Position[] {
-        return knightShifts
+        return this.directions
             .map(s => new Position(this.position.x + s.dx, this.position.y + s.dy))
             .filter(p => {
                 if (!isInsideBoard(p)) return false;
