886dc7e09b85ceefd7266c5bcbe7bd30d6b3592c
Ed Soria <edsoria76@gmail.com>
2025-08-01 10:36:36 -0600

    Add castling

--- src/core/Game.ts
+++ src/core/Game.ts
@@ -17,6 +17,19 @@ export class Game {
         this.board.movePiece(move);
         this.history.push(move);
         this.currentPlayer = getOpponentColor(this.currentPlayer);
+
+        if (move.piece.category === 'king' && Math.abs(move.from.x - move.to.x) === 2) {
+            const isKingside = move.to.x > move.from.x;
+            const row = move.from.y;
+            const rookFrom = new Position(isKingside ? 7 : 0, row);
+            const rookTo = new Position(isKingside ? 5 : 3, row);
+            const rook = this.board.getPiece(rookFrom);
+            this.board.movePiece({
+                piece: rook!,
+                from: rookFrom,
+                to: rookTo
+            });
+        }
     }
 
     getLastMove(): Move | null {
